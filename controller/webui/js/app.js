6c608894f263713badbd39a1293fe4a9b92053ee
Ben Coleman <becolem@microsoft.com>
2018-06-10 14:40:28 +0100

    Web UI update

--- controller/webui/js/app.js
+++ controller/webui/js/app.js
@@ -9,16 +9,27 @@ function pageLoad() {
       mode: "ace/mode/yaml",
       selectionStyle: "text",
       theme: "ace/theme/tomorrow_night",
-      fontSize: "19px"
+      fontSize: "19px",
+      tabSize: 2,
+      useSoftTabe: true
   })
 
   // Restore old job YAML
   let oldjob = window.localStorage.getItem('rayScaleJob');
   editor.setValue(oldjob);
-  // And update job table
+  // And update tables
   jobRefresh();
-  // Select starting page
-  selectPage('jobEdit');
+  tracerRefresh();
+
+  var url = window.location.href; 
+  hashIndex = url.indexOf('#')
+  if(hashIndex > 0) {
+    page = url.substr(hashIndex+1, url.length);
+    selectPage(page);
+  } else {
+    // Select starting page
+    selectPage('jobEdit');
+  }
 }
 
 //
@@ -32,19 +43,56 @@ function jobSubmitter() {
       method: "POST",
       body: job
   })
-  .then(res => { 
-    res.json().then(j => { document.getElementById('resp').innerHTML = JSON.stringify(j) });
-    })
-  .catch(err => {
-    err.json().then(j => { document.getElementById('resp').innerHTML = "err "+JSON.stringify(j)  });
+  .then(res => {
+    if(res.status != 200) {
+      res.json().then(data => { 
+        $('#status').html('<i class="fas fa-exclamation-triangle"></i> Job submission failed: '+data.msg)
+      });
+    } else {
+      statusBarUpdate();
+    }
   })
+  .catch(err => {
+    $('#status').html('<i class="fas fa-exclamation-triangle"></i> Error occurred submitting job!')
+  })
+}
+
+//
+// Update status bar
+//
+var updaterId
+function statusBarUpdate() {
+  fetch("/api/status", {
+      headers: { 'Accept': 'application/json' },
+      method: "GET"
+  })
+  .then(res => { 
+    res.json().then(data => { 
+      if(data.job) {
+        if(data.job.status == "RUNNING") {
+          $('#status').html(`<i class="fa fa-sync fa-spin"></i>&nbsp; Job: ${data.job.name} is running. Completed ${data.job.tasksComplete} of ${data.job.taskCount} tasks...`)
+        } else {
+          clearTimeout(updaterId);
+          $('#status').html(`Job: ${data.job.name} is ${data.job.status}. ${data.job.reason}`)
+        }
+      } else {
+        clearTimeout(updaterId);
+      }
+    });
+  })
+  .catch(err => {
+    $('#status').html('<i class="fas fa-exclamation-triangle"></i> Error occurred getting job status!')
+    clearTimeout(updaterId);
+  })
+
+  updaterId = setTimeout(statusBarUpdate, 1000);
 }
 
 //
 // Refresh job table
 //
 function jobRefresh() {
-  let table = $('#jobList'); //document.getElementById('jobList');
+  let table = $('#jobList');
   table.empty();
   table.innerHTML = "";
   fetch("/api/jobs", {
@@ -64,11 +112,43 @@ function jobRefresh() {
     });
   })
   .catch(err => {
+    $('#status').html('<i class="fas fa-exclamation-triangle"></i> Error with job listing! '+err)
+  })
+}
+
+//
+// Refresh tracer table
+//
+function tracerRefresh() {
+  let table = $('#tracerList');
+  table.empty();
+  table.innerHTML = "";
+  fetch("/api/tracers", {
+      headers: {'Accept': 'application/json'},
+      method: "GET",
+  })
+  .then(res => { 
+    res.json().then(data => {
+      for(let tracer in data) {
+        let row = $('<tr/>');
+        row.append(`<td><b>${data[tracer].endPoint}</b></td>`);
+        row.append(`<td><b>${data[tracer].id}</b></td>`);
+        table.append(row);
+      }
+      $('#tracerCount').html(Object.keys(data).length);
+    });
+  })
+  .catch(err => {
     err.json().then(j => { document.getElementById('resp').value=("err "+JSON.stringify(j)) });
   })
 }
 
 function selectPage(page) {
+  var url = window.location.href; 
+  url = url.substring(0, url.indexOf('#'));
+  window.location.href = (url + '#' + page);
+  console.log(window.location.href)
+
   $('.page').each((i, e) => {
     $(e).hide();
   });
