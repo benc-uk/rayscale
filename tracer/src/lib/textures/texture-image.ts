a9c29bb946ae10047265215aa4701f6b0b90de0c
Ben Coleman <benc.uk@gmail.com>
2020-10-04 15:01:33 +0100

    testing

--- tracer/src/lib/textures/texture-image.ts
+++ tracer/src/lib/textures/texture-image.ts
@@ -38,11 +38,11 @@ export class TextureImage extends Texture {
     // NOTE: Tiny fudge factor here is not ideal, but it prevents seams
     let ui, vi: number;
     if(this.swapUV) {
-      ui = (v * this.png.width * 0.99);
-      vi = (u * this.png.height * 0.99);
+      ui = (v * this.png.width * 1);//0.99);
+      vi = (u * this.png.height * 1);//0.99);
     } else {
-      ui = (u * this.png.width * 0.99);
-      vi = (v * this.png.height * 0.99);
+      ui = (u * this.png.width * 1);//0.99);
+      vi = (v * this.png.height * 1);//0.99);
     }
 
     // Bilinear filtering from four points
